fd5c2c93329e512fe559444a201944538b2c7119
Lucas DeVargas Souza <ls18017@gmail.com>
2019-08-12 22:04:32 -0300

    Aplicação iniciada e configurada de forma inicial a classe de contato helper.

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:agenda_contatos/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Agenda de contatos",
+    home: HomePage(),
+  ));
+}
