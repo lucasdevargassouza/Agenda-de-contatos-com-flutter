b9da0a6c74e7629e1a867f428ce0c8d3237d8d10
Lucas de Vargas Souza <lvargas@gvdasa.local>
2019-08-13 15:32:27 -0300

    Já é possível salvar um contato e lista-lo a seguir!

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,6 +7,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ContactHelper helper = ContactHelper();
+
+  @override
+  void initState() {
+    super.initState();
+
+    /* Contact c = Contact();
+    c.name = "Lucas";
+    c.email = "lucas@gmail.com";
+    c.phone = "9966332211";
+    c.img = "Teste img!";
+
+    helper.saveContact(c).then((data) {
+      helper.getAllContacts().then((list) {
+        print(list);
+      });
+    }); */
+    
+    helper.getAllContacts().then((list) {
+      print(list);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
